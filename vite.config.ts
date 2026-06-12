08a707d1b326ad63e78e817e29e41893c5653773
unknown <simondiep1@gmail.com>
2026-06-12 01:04:04 -0500

    update vite.config.ts to preserve html src and href on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,21 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-autobattler/dist',
+  build: {
+    rollupOptions: {
+      output: {
+        // Keeps the primary entry file name consistent (e.g., index.js)
+        entryFileNames: 'assets/[name].js',
+
+        // Keeps code-split chunk names consistent (e.g., vendor.js)
+        chunkFileNames: 'assets/[name].js',
+
+        // Keeps CSS and media asset names consistent (e.g., index.css)
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
